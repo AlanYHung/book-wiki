bf2bf15bf6ced0c64aa6893cdf26d08078bdbda5
James Gerstenberger <gerstej9@gmail.com>
2020-12-16 14:52:22 -0800

    Completed remaining work from yesterday including functions for sql and error callbacks, font familiy style, and modules folder

--- server.js
+++ server.js
@@ -20,24 +20,39 @@ app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
 
-
 app.get('/', getHomepage);
 app.get('/books/:details', getBookDetails);
 app.get('/searches/new', getSearches);
 app.post('/searches', callBookApi);
 app.post('/books', savedBook);
 
-function savedBook(req, res) {
+function sqlGet(req) {
   const sqlArray = [req.body.title, req.body.img_url, req.body.isbn, req.body.authors, req.body.description];
 
   const sql = 'INSERT INTO books (title, img_url, isbn, authors, book_description) VALUES ($1, $2, $3, $4, $5) RETURNING *';
-  client.query(sql, sqlArray)
-    .then(results => {
-      console.log(results.rows[0]);
-      res.redirect(`/books/${results.rows[0].id}`);
-    }).catch(error =>{
-      console.error(error);
-    });
+  return client.query(sql, sqlArray);
+}
+
+function sqlReturn() {
+  let sqlBookQuery = 'SELECT * FROM books';
+  return client.query(sqlBookQuery);
+}
+
+function sqlDetails(req) {
+  let sqlBookDetails = 'SELECT * FROM books WHERE id=$1';
+  return client.query(sqlBookDetails, [req.params.details]);
+}
+
+function catchError(error, res){
+  console.error(error);
+  res.status(500).send('We have encountered an internal service error');
+}
+
+function savedBook(req, res) {
+  sqlGet(req).then(results => {
+    console.log(results.rows[0]);
+    res.redirect(`/books/${results.rows[0].id}`);
+  }).catch(error => catchError(error, res));
 }
 
 
@@ -46,28 +61,21 @@ function getSearches(req, res) {
 }
 
 function getHomepage(req, res) {
-  let sqlBookQuery = 'SELECT * FROM books';
-  client.query(sqlBookQuery)
-    .then(results => {
-      let indexData = results.rows;
-      res.render('pages/index.ejs', { indexData: indexData });
-    })
-    .catch(error => {
-      console.error(`SQL Data retrieve Error: ${error}`);
-    });
+  sqlReturn().then(results => {
+    let indexData = results.rows;
+    res.render('pages/index.ejs', { indexData: indexData });
+  })
+    .catch(error => catchError(error, res));
 }
 
 function getBookDetails(req, res) {
   // TODO: Add an if Statement for SQL Insert
-  let sqlBookDetails = 'SELECT * FROM books WHERE id=$1';
-  client.query(sqlBookDetails, [req.params.details])
-    .then(result => {
-      const resultData = result.rows[0];
-      res.render('pages/books/show.ejs', { indexObj: resultData });
-    })
-    .catch(error => {
-      console.error(`Failure to Retrieve Book from Database.  Error: ${error}`);
-    });
+
+  sqlDetails(req).then(result => {
+    const resultData = result.rows[0];
+    res.render('pages/books/show.ejs', { indexObj: resultData });
+  })
+    .catch(error => catchError(error, res));
 }
 
 function callBookApi(req, res) {
@@ -84,10 +92,7 @@ function callBookApi(req, res) {
         return new BookObject(arrayObject);
       }));
       res.render('pages/searches/show.ejs', { books: bookArr[0] });
-    }).catch(error => {
-      console.error(error);
-      res.status(500).send('Your search returned no results.')
-    });
+    }).catch(error => catchError(error, res));
 }
 
 function BookObject(jsonBookObject) {
